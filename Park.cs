f01f6ed54e96240985551251a34350347bd164cb
Stuart Long <stuartlo@microsoft.com>
2020-04-28 16:33:41 -0700

    Add park script

--- Park.cs
+++ Park.cs
@@ -1,17 +1,80 @@
+MyIni _ini = new MyIni();
+string status = "park";
+
 public Program() {
     Runtime.UpdateFrequency = UpdateFrequency.None;
+
+    MyIniParseResult data;
+    if (!this._ini.TryParse(Me.CustomData, out data)) {
+        throw new Exception("failed to parse custom data");
+    }
 }
 
 public void Main() {
-    // lock the landing gears
-    // turn off engines
-    // connector locked
-    // turn off reactor
-    // stockpile tanks
-    // recharge batteries
-    // 
+    if (this.status == "launch") {
+        Echo("parking");
+        // lock the landing gears
+        this.RunActionOnBlocks("gears", "Lock");
+
+        // turn off engines
+        this.RunActionOnBlocks("thrusters", "OnOff_Off");
+
+        // lock connector
+        this.RunActionOnBlocks("connector", "Connect");
+        
+        // turn off reactor
+        this.RunActionOnBlocks("reactor", "OnOff_Off");
+
+        // stockpile tanks
+        this.RunActionOnBlocks("tanks", "Stockpile_On");
+
+        // recharge batteries
+        this.RunActionOnBlocks("batteries", "Recharge");
+
+        this.status = "park";
+    } else if (this.status == "park") {
+        Echo("launching");
+        // enable batteries
+        this.RunActionOnBlocks("batteries", "Auto");
+
+        // stop stockpile
+        this.RunActionOnBlocks("tanks", "Stockpile_Off");
+        
+        // turn on reactor
+        this.RunActionOnBlocks("reactor", "OnOff_On");
+
+        // ulock connector
+        this.RunActionOnBlocks("connector", "Disconnect");
+
+        // turn on engines
+        this.RunActionOnBlocks("thrusters", "OnOff_On");
+
+        // lock the landing gears
+        this.RunActionOnBlocks("gears", "Lock");
+
+        this.status = "launch";
+    } else {
+        throw new Exception("Unrecognized status " + this.status);
+    }
 }
 
-List<IMyTerminalBlock> GetBlockGroup() {
+void RunActionOnBlocks(string name, string action) {
+    string blockNames = this._ini.Get("names", name).ToString();
+    if (blockNames != null && blockNames != "") {
+        List<IMyTerminalBlock> blocks = this.GetBlockGroup(blockNames);
+        foreach(IMyTerminalBlock block in blocks) {
+            block.GetActionWithName(action).Apply(block);
+        }
+    }
+}
 
+List<IMyTerminalBlock> GetBlockGroup(string name) {
+    List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+    var pistonsBlock = GridTerminalSystem.GetBlockGroupWithName(name);
+    if (pistonsBlock) {
+        pistonsBlock.GetBlocks(blocks);
+    } else {
+        blocks.Add(GridTerminalSystem.GetBlockWithName(name));
+    } 
+    return blocks;
 }
